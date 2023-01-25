4705ab0782346aaa504ba5cfaca67441c9d27702
bruno.roma <brunoo.roma@gmail.com>
2023-01-25 00:11:52 -0300

    alerta de copiado en pantalla SobreMi

--- src/components/SobreMi/SobreMi.jsx
+++ src/components/SobreMi/SobreMi.jsx
@@ -1,5 +1,5 @@
 import "./SobreMi.css";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import fotoPerfil from "../../utils/img/fotoSobreMi.jpeg";
 import { Card, Container, Row, Col } from "react-bootstrap";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
@@ -14,13 +14,15 @@ import {
   faBootstrap,
   faLinkedin,
   faNodeJs,
-  faSass
+  faSass,
 } from "@fortawesome/free-brands-svg-icons";
 import { Button } from "react-bootstrap";
 import Modal from "react-bootstrap/Modal";
 import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
-import OverlayTrigger from 'react-bootstrap/OverlayTrigger';
-import Tooltip from 'react-bootstrap/Tooltip';
+import OverlayTrigger from "react-bootstrap/OverlayTrigger";
+import Tooltip from "react-bootstrap/Tooltip";
+import ClipboardJS from "clipboard";
+import Swal from "sweetalert2";
 
 const SobreMi = ({ greeting }) => {
   let estiloLinks = {
@@ -30,6 +32,24 @@ const SobreMi = ({ greeting }) => {
   };
 
   const [show, setShow] = useState(false);
+
+  const correoCopiado = () => {
+    Swal.fire({
+      title: "Correo Electronico Copiado",
+      text: "brunoo.roma@gmail.com",
+      confirmButtonColor: "#053D74",
+      showClass: {
+        popup: "animate__animated animate__fadeInDown",
+      },
+      hideClass: {
+        popup: "animate__animated animate__fadeOutUp",
+      },
+    });
+  };
+
+  useEffect(() => {
+    new ClipboardJS(".copy-link");
+  }, []);
 
   return (
     <div
@@ -42,25 +62,46 @@ const SobreMi = ({ greeting }) => {
       <Container className="cardSobreMi">
         <Row className="row justify-content-center align-items-center">
           <Col className="col-12 col-sm-12 col-lg-6">
-              <Card.Img src={fotoPerfil} className="fotoSobreMi" />
+            <Card.Img src={fotoPerfil} className="fotoSobreMi" />
           </Col>
           <Col className="col-12 col-sm-12 col-lg-6">
             <div className="divDescripcion">
-
+              <p>Hola! Soy Bruno, un gusto que hayas llegado a mi portfolio!</p>
               <p>
-              Hola! Soy Bruno, un gusto que hayas llegado a mi portfolio! 
-              </p>
-              <p>Espero que el próximo extracto te ayude a conocerme un poco mas y a entender mi objetivo actual</p>
-              <p>
-              Tengo más de 10 años de experiencia en el campo laboral, de los cuales los últimos años han sido relacionas al rubro de sistemas. Actualmente, estoy buscando mi primer trabajo como desarrollador front-end, de preferencia React pero no es un limitante esto</p>
-              <p>
-              Mi experiencia como analista funcional me ha dado una comprensión sólida de las metodologías ágiles, utilizando herramientas como Trello, OsTicket o Jira para la organización de tareas </p>
-              <p>Sobre mi stack tecnológico te puedo comentar que es HTML, CSS, JavaScript, React, Angular, Git (GitHub) y obviamente no puede faltar nuestro amigo Bootstrap, todo esto lo aprendí tanto con los cursos realizados en CoderHouse como tambien con capacitaciones de forma autodidacta
+                Espero que el próximo extracto te ayude a conocerme un poco mas
+                y a entender mi objetivo actual
               </p>
               <p>
-              Mi experiencia en el desarrollo de páginas web no solo es como front-end, sino que también tengo nociones de backend y estoy dispuesto a aprender más aun</p>
-              <p> 
-              Estoy seguro de que el equipo de desarrollo que me de esa oportunidad que busco no se va a arrepentir, ya que estoy con muchas ganas y muy emocionado de poder aplicar mi experiencia y mis habilidades en mi próximo desafio profesional, como asi tambien aprender de ellos cosas nuevas y a mejorar lo que ya se
+                Tengo más de 10 años de experiencia en el campo laboral, de los
+                cuales los últimos años han sido relacionas al rubro de
+                sistemas. Actualmente, estoy buscando mi primer trabajo como
+                desarrollador front-end, de preferencia React pero no es un
+                limitante esto
+              </p>
+              <p>
+                Mi experiencia como analista funcional me ha dado una
+                comprensión sólida de las metodologías ágiles, utilizando
+                herramientas como Trello, OsTicket o Jira para la organización
+                de tareas{" "}
+              </p>
+              <p>
+                Sobre mi stack tecnológico te puedo comentar que es HTML, CSS,
+                JavaScript, React, Angular, Git (GitHub) y obviamente no puede
+                faltar nuestro amigo Bootstrap, todo esto lo aprendí tanto con
+                los cursos realizados en CoderHouse como tambien con
+                capacitaciones de forma autodidacta
+              </p>
+              <p>
+                Mi experiencia en el desarrollo de páginas web no solo es como
+                front-end, sino que también tengo nociones de backend y estoy
+                dispuesto a aprender más aun
+              </p>
+              <p>
+                Estoy seguro de que el equipo de desarrollo que me de esa
+                oportunidad que busco no se va a arrepentir, ya que estoy con
+                muchas ganas y muy emocionado de poder aplicar mi experiencia y
+                mis habilidades en mi próximo desafio profesional, como asi
+                tambien aprender de ellos cosas nuevas y a mejorar lo que ya se
               </p>
             </div>
             <Button
@@ -79,9 +120,13 @@ const SobreMi = ({ greeting }) => {
             >
               <Modal.Header closeButton>
                 <Modal.Title id="example-custom-modal-styling-title">
-                  <h2 style={{
-                    fontWeight: 'bold'
-                  }}>Medios de Contacto</h2>
+                  <h2
+                    style={{
+                      fontWeight: "bold",
+                    }}
+                  >
+                    Medios de Contacto
+                  </h2>
                 </Modal.Title>
               </Modal.Header>
               <Modal.Body>
@@ -108,7 +153,13 @@ const SobreMi = ({ greeting }) => {
                       icon={faGithub}
                     />
                   </a>
-                  <a href="mailto:brunoo.roma@gmail.com" style={estiloLinks}>
+                  <a
+                    onClick={correoCopiado}
+                    href="#"
+                    className="copy-link"
+                    style={estiloLinks}
+                    data-clipboard-text="brunoo.roma@gmail.com"
+                  >
                     <FontAwesomeIcon
                       className="contenedorRedesModal iconoEmail"
                       icon={faEnvelope}
@@ -133,165 +184,164 @@ const SobreMi = ({ greeting }) => {
           <Row className="row">
             <Col className="col-12 col-sm-0 col-lg-1"></Col>
             <Col className="col-6 col-sm-4 col-lg-1">
-            <OverlayTrigger
-          key='bottom'
-          placement='bottom'
-          overlay={
-            <Tooltip id='tooltip-bottom'>
-              <strong>HTML5</strong>
-            </Tooltip>
-          }
-        >
-                 <FontAwesomeIcon
-                icon={faHtml5}
-                className="logoTecnologia logoHtml"
-              />
-        </OverlayTrigger>
- 
+              <OverlayTrigger
+                key="bottom"
+                placement="bottom"
+                overlay={
+                  <Tooltip id="tooltip-bottom">
+                    <strong>HTML5</strong>
+                  </Tooltip>
+                }
+              >
+                <FontAwesomeIcon
+                  icon={faHtml5}
+                  className="logoTecnologia logoHtml"
+                />
+              </OverlayTrigger>
             </Col>
             <Col className="col-6 col-sm-4 col-lg-1">
-            <OverlayTrigger
-          key='bottom'
-          placement='bottom'
-          overlay={
-            <Tooltip id='tooltip-bottom'>
-              <strong>CSS3</strong>
-            </Tooltip>
-          }
-        >
-              <FontAwesomeIcon
-                icon={faCss3}
-                className="logoTecnologia logoCss"
-              />
-        </OverlayTrigger>
+              <OverlayTrigger
+                key="bottom"
+                placement="bottom"
+                overlay={
+                  <Tooltip id="tooltip-bottom">
+                    <strong>CSS3</strong>
+                  </Tooltip>
+                }
+              >
+                <FontAwesomeIcon
+                  icon={faCss3}
+                  className="logoTecnologia logoCss"
+                />
+              </OverlayTrigger>
             </Col>
             <Col className="col-6 col-sm-4 col-lg-1">
-            <OverlayTrigger
-          key='bottom'
-          placement='bottom'
-          overlay={
-            <Tooltip id='tooltip-bottom'>
-              <strong>Javascript</strong>
-            </Tooltip>
-          }
-        >
-              <FontAwesomeIcon
-                icon={faSquareJs}
-                className="logoTecnologia logoJs"
-              />
-        </OverlayTrigger>
+              <OverlayTrigger
+                key="bottom"
+                placement="bottom"
+                overlay={
+                  <Tooltip id="tooltip-bottom">
+                    <strong>Javascript</strong>
+                  </Tooltip>
+                }
+              >
+                <FontAwesomeIcon
+                  icon={faSquareJs}
+                  className="logoTecnologia logoJs"
+                />
+              </OverlayTrigger>
             </Col>
             <Col className="col-6 col-sm-4 col-lg-1">
-            <OverlayTrigger
-          key='bottom'
-          placement='bottom'
-          overlay={
-            <Tooltip id='tooltip-bottom'>
-              <strong>React</strong>
-            </Tooltip>
-          }
-        >
-              <FontAwesomeIcon
-                icon={faReact}
-                className="logoTecnologia logoReact"
-              />
-        </OverlayTrigger>
+              <OverlayTrigger
+                key="bottom"
+                placement="bottom"
+                overlay={
+                  <Tooltip id="tooltip-bottom">
+                    <strong>React</strong>
+                  </Tooltip>
+                }
+              >
+                <FontAwesomeIcon
+                  icon={faReact}
+                  className="logoTecnologia logoReact"
+                />
+              </OverlayTrigger>
             </Col>
             <Col className="col-6 col-sm-4 col-lg-1">
-            <OverlayTrigger
-          key='bottom'
-          placement='bottom'
-          overlay={
-            <Tooltip id='tooltip-bottom'>
-              <strong>Angular</strong>
-            </Tooltip>
-          }
-        >
-              <FontAwesomeIcon
-                icon={faAngular}
-                className="logoTecnologia logoAngular"
-              />
-        </OverlayTrigger>
+              <OverlayTrigger
+                key="bottom"
+                placement="bottom"
+                overlay={
+                  <Tooltip id="tooltip-bottom">
+                    <strong>Angular</strong>
+                  </Tooltip>
+                }
+              >
+                <FontAwesomeIcon
+                  icon={faAngular}
+                  className="logoTecnologia logoAngular"
+                />
+              </OverlayTrigger>
             </Col>
             <Col className="col-6 col-sm-4 col-lg-1">
-            <OverlayTrigger
-          key='bottom'
-          placement='bottom'
-          overlay={
-            <Tooltip id='tooltip-bottom'>
-              <strong>Bootstrap</strong>
-            </Tooltip>
-          }
-        >
-              <FontAwesomeIcon
-                icon={faBootstrap}
-                className="logoTecnologia logoBootstrap"
-              />
-        </OverlayTrigger>
+              <OverlayTrigger
+                key="bottom"
+                placement="bottom"
+                overlay={
+                  <Tooltip id="tooltip-bottom">
+                    <strong>Bootstrap</strong>
+                  </Tooltip>
+                }
+              >
+                <FontAwesomeIcon
+                  icon={faBootstrap}
+                  className="logoTecnologia logoBootstrap"
+                />
+              </OverlayTrigger>
             </Col>
             <Col className="col-6 col-sm-4 col-lg-1">
-            <OverlayTrigger
-          key='bottom'
-          placement='bottom'
-          overlay={
-            <Tooltip id='tooltip-bottom'>
-              <strong>Git</strong>
-            </Tooltip>
-          }
-        >
-              <FontAwesomeIcon
-                icon={faGitAlt}
-                className="logoTecnologia logoGit"
-              />
-        </OverlayTrigger>
+              <OverlayTrigger
+                key="bottom"
+                placement="bottom"
+                overlay={
+                  <Tooltip id="tooltip-bottom">
+                    <strong>Git</strong>
+                  </Tooltip>
+                }
+              >
+                <FontAwesomeIcon
+                  icon={faGitAlt}
+                  className="logoTecnologia logoGit"
+                />
+              </OverlayTrigger>
             </Col>
             <Col className="col-6 col-sm-4 col-lg-1">
-            <OverlayTrigger
-          key='bottom'
-          placement='bottom'
-          overlay={
-            <Tooltip id='tooltip-bottom'>
-              <strong>Github</strong>
-            </Tooltip>
-          }
-        >
-              <FontAwesomeIcon
-                icon={faGithub}
-                className="logoTecnologia logoGithub"
-              />
-        </OverlayTrigger>
+              <OverlayTrigger
+                key="bottom"
+                placement="bottom"
+                overlay={
+                  <Tooltip id="tooltip-bottom">
+                    <strong>Github</strong>
+                  </Tooltip>
+                }
+              >
+                <FontAwesomeIcon
+                  icon={faGithub}
+                  className="logoTecnologia logoGithub"
+                />
+              </OverlayTrigger>
             </Col>
             <Col className="col-6 col-sm-4 col-lg-1">
-            <OverlayTrigger
-          key='bottom'
-          placement='bottom'
-          overlay={
-            <Tooltip id='tooltip-bottom'>
-              <strong>NodeJS</strong>
-            </Tooltip>
-          }
-        >
-              <FontAwesomeIcon
-                icon={faNodeJs}
-                className="logoTecnologia logoNodeJS"
-              />
-        </OverlayTrigger>
+              <OverlayTrigger
+                key="bottom"
+                placement="bottom"
+                overlay={
+                  <Tooltip id="tooltip-bottom">
+                    <strong>NodeJS</strong>
+                  </Tooltip>
+                }
+              >
+                <FontAwesomeIcon
+                  icon={faNodeJs}
+                  className="logoTecnologia logoNodeJS"
+                />
+              </OverlayTrigger>
             </Col>
             <Col className="col-6 col-sm-4 col-lg-1">
-            <OverlayTrigger
-          key='bottom'
-          placement='bottom'
-          overlay={
-            <Tooltip id='tooltip-bottom'>
-              <strong>Sass</strong>
-            </Tooltip>
-          }
-        >
-              <FontAwesomeIcon
-                icon={faSass}
-                className="logoTecnologia logoSass"
-              />
-        </OverlayTrigger>
+              <OverlayTrigger
+                key="bottom"
+                placement="bottom"
+                overlay={
+                  <Tooltip id="tooltip-bottom">
+                    <strong>Sass</strong>
+                  </Tooltip>
+                }
+              >
+                <FontAwesomeIcon
+                  icon={faSass}
+                  className="logoTecnologia logoSass"
+                />
+              </OverlayTrigger>
             </Col>
             <Col className="col-12 col-sm-0 col-lg-1"></Col>
           </Row>
