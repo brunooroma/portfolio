d3ea034019a27c06259b716e508aae2bc1d143d9
bruno.roma <brunoo.roma@gmail.com>
2023-02-03 21:39:06 -0300

    agregue el quinto proyecto

--- src/components/Data/data.js
+++ src/components/Data/data.js
@@ -1,32 +1,44 @@
 const dataProyectos = [
-    {
-        id: 1,
-        titulo: 'DC GYM',
-        imagen: '/static/mindcgym.png',
-        url: 'https://brunooroma.github.io/dcgym/',
-        descripcion: 'Sitio Web estatico de un Gimnasio, utilizando HTML, CSS y Bootstrap'
-    },
-    {
-        id: 2,
-        titulo: 'Sistema de Turnos',
-        imagen: '/static/minfichasmedicas.png',
-        url: 'https://brunooroma.github.io/fichasMedicas/',
-        descripcion: 'ABM diseñado con Javascript Vanilla, en el cual se pueden agendar turnos'
-    },
-    {
-        id: 3,
-        titulo: 'Poke E-commerce',
-        imagen: '/static/minpokecommerce.png',
-        url: 'https://pokeecommerce.netlify.app/',
-        descripcion: 'E-commerce diseñado con React, utilizando Create-react-app y publicado en Netlify'
-    },
-    {
-        id: 4,
-        titulo: 'Crud Context App',
-        imagen: '/static/mincrud.png',
-        url: 'https://brunooroma.github.io/crud-context-app/',
-        descripcion: 'CRUD de tareas, creado con ViteJS, utilizando useContext y useReducer, publicado en Github Pages'
-    }
-]
+  {
+    id: 5,
+    titulo: "Rick and Morty API",
+    imagen: "/static/minrickmorty.png",
+    url: "https://rick-and-morty-api-xi.vercel.app/",
+    descripcion:
+      "Proyecto creado con ViteJS, que utiliza la API de Rick and Morty para mostrar todos los personajes de la serie por paginas. Utilice useContext para el manejo de estados y ASYNC/AWAIT para llamar a la API. Publicado en Vercel ",
+  },
+  {
+    id: 4,
+    titulo: "Crud Context App",
+    imagen: "/static/mincrud.png",
+    url: "https://brunooroma.github.io/crud-context-app/",
+    descripcion:
+      "CRUD de tareas, creado con ViteJS, utilizando useContext y useReducer, publicado en Github Pages",
+  },
+  {
+    id: 1,
+    titulo: "DC GYM",
+    imagen: "/static/mindcgym.png",
+    url: "https://brunooroma.github.io/dcgym/",
+    descripcion:
+      "Sitio Web estatico de un Gimnasio, utilizando HTML, CSS y Bootstrap",
+  },
+  {
+    id: 3,
+    titulo: "Poke E-commerce",
+    imagen: "/static/minpokecommerce.png",
+    url: "https://pokeecommerce.netlify.app/",
+    descripcion:
+      "E-commerce diseñado con React, utilizando Create-react-app y publicado en Netlify",
+  },
+  {
+    id: 2,
+    titulo: "Sistema de Turnos",
+    imagen: "/static/minfichasmedicas.png",
+    url: "https://brunooroma.github.io/fichasMedicas/",
+    descripcion:
+      "ABM diseñado con Javascript Vanilla, en el cual se pueden agendar turnos",
+  },
+];
 
 export default dataProyectos;
